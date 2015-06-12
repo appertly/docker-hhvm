b3f1ec7372283e14098fa98e1489bdfbe483b1b9
Jonathan Hawk <jonathan@libreworks.com>
2015-06-12 16:06:42 -0400

    Adding Docker files

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM ubuntu:14.04
+MAINTAINER Jonathan Hawk <jonathan@appertly.com>
+
+ENV HHVM_VERSION 3.7.1-1~trusty
+
+# Install HHVM
+RUN apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449 \
+    && echo deb http://dl.hhvm.com/ubuntu trusty main | tee /etc/apt/sources.list.d/hhvm.list \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends ca-certificates hhvm=$HHVM_VERSION \
+    && rm -rf /var/lib/apt/lists/*
+
+ADD php.ini /etc/hhvm/php.ini
+ADD server.ini /etc/hhvm/server.ini
+
+# forward request and error logs to docker log collector
+RUN ln -sf /dev/stderr /var/log/hhvm/error.log
+
+ADD start.sh /scripts/start.sh
+RUN chmod +x /scripts/start.sh
+
+# HHVM on FastCGI with default port of 9000
+EXPOSE 9000
+
+# Default command
+CMD ["/scripts/start.sh"]
---
+++ start.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+if [ "$MEMCACHED" != "" ]; then
+    sed -i "s/handler = files/handler = memcached/" /etc/hhvm/php.ini
+    sed -i "s/\/var\/lib\/hhvm\/sessions/$MEMCACHED/" /etc/hhvm/php.ini
+fi
+
+DAEMON=/usr/bin/hhvm
+PIDFILE=/var/run/hhvm/pid
+CONFIG_FILE="/etc/hhvm/server.ini"
+SYSTEM_CONFIG_FILE="/etc/hhvm/php.ini"
+RUN_AS_USER="www-data"
+RUN_AS_GROUP="www-data"
+ADDITIONAL_ARGS=""
+
+DAEMON_ARGS="--config ${SYSTEM_CONFIG_FILE} \
+--config ${CONFIG_FILE} \
+--user ${RUN_AS_USER} \
+--mode server \
+${ADDITIONAL_ARGS}"
+
+#
+# Function that starts the daemon/service
+#
+check_run_dir() {
+    # Only perform folder creation, if the PIDFILE location was not modified
+    PIDFILE_BASEDIR=$(dirname ${PIDFILE})
+    # We might have a tmpfs /var/run.
+    if [ "/var/run/hhvm" = "${PIDFILE_BASEDIR}" ] && [ ! -d /var/run/hhvm ]; then
+        mkdir -p -m0755 /var/run/hhvm
+        chown $RUN_AS_USER:$RUN_AS_GROUP /var/run/hhvm
+    fi
+}
+
+check_run_dir
+touch $PIDFILE
+chown $RUN_AS_USER:$RUN_AS_GROUP $PIDFILE
+exec $DAEMON $DAEMON_ARGS
